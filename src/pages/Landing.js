5b62ae4714bab3e056667fe96eef3fe383ffc39b
Robspin <robin_steeman@hotmail.com>
2021-06-17 13:34:23 +0200

    added some depencies and started on example layout for landing

---
+++ src/pages/Landing.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Landing = () => {
+   return (
+      <div className='landing__background'>
+         <h2 className='landing__greentemple'>GreenTemple</h2>
+      </div>
+   );
+};
+
+export default Landing;
